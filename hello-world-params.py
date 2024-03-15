ef3ac8af53e860ed56fa3dc3d1704bd14ae15a87
joshuaiokua <jiokua.albano@gmail.com>
2024-03-14 23:23:43 -0700

    Create hello-world-params.py

---
+++ hello-world-params.py
@@ -0,0 +1,7 @@
+# Required parameters for the RAG
+REQUIRED_FIMIO_PARAMS = {
+    "planet": "Mars",
+    "message": "We come in peace",
+}
+
+print(f"Hello from {REQUIRED_FIMIO_PARAMS['planet']}! {REQUIRED_FIMIO_PARAMS['message']}!")
